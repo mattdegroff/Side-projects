53652aa62f59b8ce8e6676882ef8a9e1b3ace817
mattdegroff <30853606+mattdegroff@users.noreply.github.com>
2017-11-16 00:23:12 -0500

    Create populationCount.cpp
    In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
    
    At the end of the first year there will be: 
    1000 + 1000 * 0.02 + 50 => 1070 inhabitants
    
    At the end of the 2nd year there will be: 
    1070 + 1070 * 0.02 + 50 => 1141 inhabitants (number of inhabitants is an integer)
    
    At the end of the 3rd year there will be:
    1141 + 1141 * 0.02 + 50 => 1213
    
    It will need 3 entire years.
    More generally given parameters:
    
    p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
    
    the function nb_year should return n number of entire years needed to get a population greater or equal to p.
    
    aug is an integer, percent a positive or null number, p0 and p are positive integers (> 0)
    
    Examples:
    nb_year(1500, 5, 100, 5000) -> 15
    nb_year(1500000, 2.5, 10000, 2000000) -> 10
    Note: Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.

---
+++ populationCount.cpp
@@ -0,0 +1,32 @@
+class Arge
+{
+  public:
+  static int nbYear(int p0, double percent, int aug, int p);
+};
+
+int Arge::nbYear(int p0, double percent, int aug, int p){
+    int year = 0; //num of years to return
+    while (p0 <= p) { //checks p0 is still less than p
+      p0 = p0 + (p0 * (percent/100)) + aug; //increases population according to the model
+      year++; //adds a year for the year that just passed
+    }
+    return year; //returns the num of years
+  }
+  
+void testequal(int ans, int sol) {
+    Assert::That(ans, Equals(sol));
+}
+void dotest(int p0, double percent, int aug, int p, int expected)
+{
+    testequal(Arge::nbYear(p0, percent, aug, p), expected);
+}
+Describe(nbYear_Tests)
+{
+    It(Fixed__Tests)
+    {
+        dotest(1500, 5, 100, 5000,15);
+        dotest(1500000, 2.5, 10000, 2000000, 10);
+        dotest(1500000, 0.25, 1000, 2000000, 94);
+        dotest(1500000, 0.25, -1000, 2000000, 151);
+    }
+};
